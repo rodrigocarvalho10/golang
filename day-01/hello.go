2c2b9d609026a0ba53cc39c6b73c30a0a64f6685
rodrigocarvalho10 <rodrigo.carvalho.it@gmail.com>
2023-11-14 18:25:42 -0300

    Adicionando o Day 01 - Hello World em Go

---
+++ day-01/hello.go
@@ -0,0 +1,12 @@
+/* Programa em Go do Padrão Hello World
+   Simulando correção - Hotfix
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	msg := "Hello World"
+	fmt.Println("Meu primeiro", msg, "em Go")
+}
