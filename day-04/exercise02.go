6d3a0bc8e918db816858b4b3b6181a0ccf6d3acf
rodrigocarvalho10 <rodrigo.carvalho.it@gmail.com>
2023-11-15 14:58:37 -0300

    Adicionando Exercício 02

---
+++ day-04/exercise02.go
@@ -0,0 +1,33 @@
+/*
+Escreva expressões utilizando os seguintes operadores, e atribua seus valores a variáveis.
+° ==
+° !=
+° <=
+° <
+° >=
+° >
+° =
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := (9 == 9)
+	b := (9 != 9)
+	c := (9 <= 9)
+	d := (9 < 9)
+	e := (9 >= 9)
+	f := (9 > 9)
+
+	fmt.Printf("Valor de A é: %v\n", a)
+	fmt.Printf("Valor de B é: %v\n", b)
+	fmt.Printf("Valor de C é: %v\n", c)
+	fmt.Printf("Valor de D é: %v\n", d)
+	fmt.Printf("Valor de E é: %v\n", e)
+	fmt.Printf("Valor de F é: %v", f)
+
+}
