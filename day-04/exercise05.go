e210ca366c9b3559f52d17371bda7d7e530a41d6
rodrigocarvalho10 <rodrigo.carvalho.it@gmail.com>
2023-11-15 15:34:54 -0300

    Exercício 05

---
+++ day-04/exercise05.go
@@ -0,0 +1,17 @@
+/*
+Crie uma variável de tipo string utilizando uma raw string literal
+Demonstre-a
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := `Aprendendo
+	Golang
+		de forma sensacional`
+	fmt.Println(x)
+}
